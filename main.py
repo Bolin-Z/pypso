dee05871be27a53c01dd60bc454e726a189e94b2
BolinZ <972497936@qq.com>
2023-05-12 02:12:04 +0800

    set run times

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 if __name__ == "__main__":
     # lab parameters
     dimension = 10
-    runNumber = 30
+    runNumber = 50
     sampleFEsPoints:list[float] = [0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     # define benchmark functions
     problems = {
